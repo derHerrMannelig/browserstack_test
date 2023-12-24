37a0b98fe01c6d484ea88376810e7e632d073854
Ilya Tyupo <derherrmannelig@gmail.com>
2023-12-24 18:52:36 +0200

    feat: add 5th test case

---
+++ test/specs/invalid.sign.in.js
@@ -0,0 +1,19 @@
+const page = require('../pages/page.js');
+const homePage = require('../pages/home.page.js');
+const loginPage = require('../pages/login.page.js');
+const { faker } = require('@faker-js/faker');
+
+const randomEmail = faker.internet.email();
+const randomPassword = faker.internet.password();
+
+describe('Sign up / sign in', () => {
+    it('should verify registration and login', async () => {
+        await page.touch(homePage.loginBtn);
+        await expect(loginPage.loginTitle).toBeDisplayed();
+        await expect(loginPage.loginTitle).toHaveText('Login / Sign up Form');
+        await loginPage.fillCredentials(randomEmail, randomPassword);
+        await page.touch(loginPage.btnLogin);
+        await expect(loginPage.alertSuccess).toBeDisplayed();
+        await expect(loginPage.alertSuccess).toHaveText('Error!');
+    })
+})
